9ea393b89eb127d102dc964272406db976d094ec
abiolasikal <sikal.sikal.ss@gmail.com>
2022-08-07 02:13:42 +0100

    Project Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import Header from "./components/Header/Header";
+import {ThemeProvider} from "styled-components";
+import {theme} from "./styles/ThemeProvider";
+import GlobalStyles from "./styles/GlobalStyles";
+import Card from "./components/Card/Card";
+import content from "./data";
+import {Container} from "./styles";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  return (
+    <ThemeProvider theme={theme}>
+        <GlobalStyles/>
+        <Header />
+        <Container>
+            {content.map((item) => <Card key={item.id} item={item} />)}
+        </Container>
+        <Footer />
+    </ThemeProvider>
   );
 }
 
